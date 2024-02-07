6ebdbd0bb109b9354a015d61b10f82701a3764a8
Yuriy <aka4an@gmail.com>
2024-02-07 20:26:24 +0700

    Add bullet to a root scene.

--- components/weapon_component.gd
+++ components/weapon_component.gd
@@ -17,6 +17,6 @@ func shoot(shooting_point: Vector2, rotation: float):
 	new_bullet.global_position = shooting_point
 	new_bullet.global_rotation = rotation
 	new_bullet.set_collision_mask_value(collision_mask, true)
-	get_parent().add_child(new_bullet)
+	get_tree().root.add_child(new_bullet)
 	
 	get_tree().create_timer(cooldown_time).timeout.connect(func(): _is_on_cooldown = false)

9e2fbdb228c7841812425e62752314926a1c8c4b
Yuriy <aka4an@gmail.com>
2024-02-18 01:00:54 +0700

    Make powerups fly

--- game/powerup/powerup.gd
+++ game/powerup/powerup.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 var _is_flying: bool = true
 var _is_weapon_revealed: bool = false
+var _start_position: Vector2
 
 var _guns = [ 
 		"res://game/weapons/machine_gun.tscn", 
@@ -12,8 +13,17 @@ var _guns = [
 	]
 var _weapon_index = -1
 
+
+func _ready():
+	_start_position = global_position
+
+
 func _physics_process(delta):
-	if not _is_flying:
+	if _is_flying:
+		var speed = 50
+		global_position.x += speed * delta
+		global_position.y = _start_position.y + 15 * sin(0.1 * global_position.x)
+	else:
 		velocity.y += 10
 		if velocity.y > 1000:
 			velocity.y = 1000
@@ -30,12 +40,14 @@ func reveal_weapon():
 	if (_is_weapon_revealed):
 		return
 	_is_weapon_revealed = true
+	set_collision_mask_value(1, true)
 	_weapon_index = randi() % _guns.size()
 	sprite.frame = _weapon_index + 1
 
 
 func _on_pickup_area_area_entered(area):
-	assert(_weapon_index != -1)
+	if !_is_weapon_revealed:
+		return
 	if area.get_parent() is Player:
 		var player: Player = area.get_parent()
 		var gun = load(_guns[_weapon_index]).instantiate()

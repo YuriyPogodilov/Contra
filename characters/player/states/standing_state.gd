978b1d9cf57341c21d877332efffe43522f6460a
Yuriy <aka4an@gmail.com>
2024-02-25 20:40:16 +0700

    Use direction in states

--- characters/player/states/standing_state.gd
+++ characters/player/states/standing_state.gd
@@ -35,9 +35,4 @@ func update(delta):
 			_player.get_sprite().frame = 14
 
 	if is_shooting:
-		var rotation = 0
-		if _player.get_sprite().flip_h:
-			rotation = deg_to_rad(180)
-		if direction.y < 0:
-			rotation = deg_to_rad(-90)
-		_player.shoot(Vector2.RIGHT.rotated(rotation))
+		_player.shoot(direction)
